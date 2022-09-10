1e5b62952045641d17983b61450695defe07e9d0
shreyashdj <shreyashjadhav1799@gmail.com>
2022-09-11 00:29:55 +0530

    [Shreyash]: Added code to create Node class to store data and next variable

---
+++ src/com/bridgelabz/stacksAndQueues/Node.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.stacksAndQueues;
+
+/*
+ * @author SHREYASH JADHAV
+ */
+public class Node {
+	
+	// variables
+	
+		int data;
+		Node next;
+		
+		// Constructor
+		
+		public Node(int data) {
+
+			this.data = data;
+			this.next = null;
+		}
+	
+
+}
