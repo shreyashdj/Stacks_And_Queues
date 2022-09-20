dc3ecd5675159955a88548c370ae65a4315f7a4e
shreyashdj <shreyashjadhav1799@gmail.com>
2022-09-20 06:44:57 +0530

    [Shreyash]: Added code to create and use Stack methods peek, pop on Linked List

--- src/com/bridgelabz/stacksAndQueues/Main.java
+++ src/com/bridgelabz/stacksAndQueues/Main.java
@@ -6,15 +6,40 @@ package com.bridgelabz.stacksAndQueues;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		/*
+		 * 	Stack Methods
+		 */
+		
+		System.out.println("\n **** Stack Methods ****");
+		
 		StackOperation stackOperation = new StackOperation();	// created object of StackOperation class
+		
+		System.out.println("\n **** Pushing Operation ****");
+		System.out.println("\n **** Pushing 70 then 30 then 56 ****");
+
+		/*
+		 * 	pushing 70, 30, 56 using push method
+		 */
 		
 		stackOperation.push(70);
 		stackOperation.push(30);
 		stackOperation.push(56);
 		
-		stackOperation.display();
+		stackOperation.display();		//	printing Linked List using display method
 		
+		System.out.println("\n **** Popping Operation ****");
+		
+		/*
+		 * 	used pop and display method in while loop to
+		 *  pop data and print Linked List till it gets empty
+		 */
+		
+		while ( !stackOperation.isEmpty() ) {
+			stackOperation.pop();
+			stackOperation.display();
+		}
+
 	}
 
 }
--- src/com/bridgelabz/stacksAndQueues/StackOperation.java
+++ src/com/bridgelabz/stacksAndQueues/StackOperation.java
@@ -5,7 +5,7 @@ package com.bridgelabz.stacksAndQueues;
  */
 public class StackOperation {
 	
-		Node top;	// variable
+	Node top;	// variable
 
 	/*
 	 *	 method to print Linked List
@@ -16,27 +16,27 @@ public class StackOperation {
 		Node temp = top;
 
 	    if(temp == null) {
-	    	System.out.println("\n Stack is empty");
+	    	System.out.println("\n Linked List is empty");
 	    } else {
-	    	System.out.print("\n Stack List : ");
+	    	System.out.print("\n Linked List : ");
 	    	while (temp != null) {
 	    		System.out.print(temp.data + " -> ");
 	    		temp = temp.next;
 	    	}
 	    	System.out.print("null");
 		}
-		
+		System.out.println();
 	}
 
 	/*
-	 * 	method to add data in Stack
+	 * 	method to add data in Stack at top (head)
 	 */
 	
 	public void push(int data) {
-		
+
 		Node newNode = new Node(data);		// created object of Node class & initialize with parameter data
 		
-		if (top == null) {
+		if (isEmpty()) {
 			
 			top = newNode;
 			
@@ -48,4 +48,53 @@ public class StackOperation {
 		
 	}
 
+	/*
+	 * 	method to return data at top of Stack
+	 */
+	
+	public int peek() {
+		
+		if (isEmpty()) {
+			
+			System.out.println("\n Linked List is empty");
+			return -1;
+		}
+		
+		return top.data;
+	}
+
+	/*
+	 * 	method to pop / delete data at top of Stack
+	 */
+	
+	public void pop() {
+		
+		if (peek() == -1) {
+			
+			System.out.println("\n Linked List is empty");
+			
+		} else {
+		
+			int popped = peek();		// storing popped data
+			top = top.next;
+			System.out.println("\n Popped data : " + popped);
+		}
+		
+		
+	}
+
+	/*
+	 *	method to check Linked List is empty or not
+	 */
+	
+	public boolean isEmpty() {
+		
+		if (top == null) {
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
 }
